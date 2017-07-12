db4294aaa9c69b4f15caa6e81627a9f605710fdf
forrestsill <forrestsill@gmail.com>
2017-07-12 16:48:32 -0400

    Fixed colors

--- app/static/main.js
+++ app/static/main.js
@@ -3,13 +3,13 @@ $(document).ready(function() {
         window.open($(this).data("href"), "_blank");
     });
     $(".clickable-row").each(function() {
-      var value = $(this).data("value");
+      var value = parseFloat($(this).data("value"));
       if (value > 0) {
-        $(this).css('background-color', 'rgba(0,' + Math.round(value * 255) + ',0,0.6)');
+        $(this).css("background-color", "rgba(" + (255 - Math.round(value * 255)) + ",255,0,0.6)");
       } else if (value < 0) {
-        $(this).css("background-color", "rgba(" + Math.round(-value * 255) + ",0,0,0.6)");
+        $(this).css("background-color", "rgba(255," + Math.round((value+1) * 255) + ",0,0.6)");
       } else {
-        $(this).css("background-color", "rgba(255,255,255,1)");
+        $(this).css("background-color", "rgba(255,255,0,0.6)");
       }
     });
 });
