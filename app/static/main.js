090de07d79c798b7d855e1cff55558755c72bd40
forrestsill <forrestsill@gmail.com>
2017-07-12 15:54:04 -0400

    fixes

--- app/static/main.js
+++ app/static/main.js
@@ -1,15 +1,15 @@
-$(document).ready(function($) {
+$(document).ready(function() {
     $(".clickable-row").click(function() {
         window.open($(this).data("href"), "_blank");
     });
     $(".clickable-row").each(function() {
-      var value = $(this).data(value);
+      var value = $(this).data("value");
       if (value > 0) {
-        $(this).css({"background-color": "rgba(0," + (value * 255) + ",0,0.6)"});
+        $(this).css('background-color', 'rgba(0,' + Math.round(value * 255) + ',0,0.6)');
       } else if (value < 0) {
-        $(this).css({"background-color": "rgba(" + (-value * 255) + ",0,0,0.6)"});
+        $(this).css("background-color", "rgba(" + Math.round(-value * 255) + ",0,0,0.6)");
       } else {
-        $(this).css({"background-color": "white"});
+        $(this).css("background-color", "rgba(255,255,255,1)");
       }
     });
 });
