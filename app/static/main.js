fd0475dbca6dda8280d427a9481c16d76eef076b
forrestsill <forrestsill@gmail.com>
2017-07-12 14:43:51 -0400

    Small changes

--- app/static/main.js
+++ app/static/main.js
@@ -3,15 +3,13 @@ $(document).ready(function($) {
         window.open($(this).data("href"), "_blank");
     });
     $(".clickable-row").each(function() {
-      value = .8;
-      // var value = $(this).data(value);
+      var value = $(this).data(value);
       if (value > 0) {
         $(this).css({"background-color": "rgba(0," + (value * 255) + ",0,0.6)"});
       } else if (value < 0) {
         $(this).css({"background-color": "rgba(" + (-value * 255) + ",0,0,0.6)"});
       } else {
-        $(this).css({"background-color": "rgba(0,0,0,0.5)"});
+        $(this).css({"background-color": "white"});
       }
-
-    })
+    });
 });
